6290020c330c797d00ff8c0a01afc88c5bafcc84
sektor7k <omeraydin2112@gmail.com>
2025-05-06 00:48:18 +0300

    multi client

---
+++ src/bin/client.rs
@@ -0,0 +1,62 @@
+use mini_redis::client;
+use bytes::Bytes;
+use tokio::sync::mpsc;
+
+#[derive(Debug)]
+enum Command {
+    Get {
+        key:String,
+    },
+    Set {
+        key:String,
+        value:Bytes,
+    }
+}
+
+
+#[tokio::main]
+async fn main(){
+    
+    let (tx, mut rx) = mpsc::channel(32);
+    let tx2 = tx.clone();
+
+    let manager = tokio::spawn(async move {
+
+        let mut client = client::connect("127.0.0.1:6379").await.unwrap();
+
+        while let Some(cmd) = rx.recv().await{
+            use Command::*;
+
+            match cmd {
+                Get {key} => {
+                    client.get(&key).await;
+                }
+                Set {key,value} => {
+                    client.set(&key, value).await;
+                }
+            }
+        }
+    });
+
+    let t1 = tokio::spawn(async move {
+        let cmd = Command::Get {
+            key:"foo".to_string(),
+        };
+
+        tx.send(cmd).await.unwrap();
+    });
+
+    let t2 = tokio::spawn(async move {
+        let cmd = Command::Set {
+            key:"foo".to_string(),
+            value:"bar".into(),
+        };
+
+        tx2.send(cmd).await.unwrap();
+    });
+
+
+    t1.await.unwrap();
+    t2.await.unwrap();
+    manager.await.unwrap();
+}
